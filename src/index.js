c67d569c35c9fec5eb327f928a44328de40ad77f
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2022-11-14 16:34:14 -0300

    [feature] post and get participants

--- src/index.js
+++ src/index.js
@@ -1,22 +1,90 @@
 import express from 'express'
 import cors from 'cors'
-import { users, tweets } from './mock.js'
-import { verifyURL } from './functions.js';
+import joi from 'joi';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv'
+import dayjs from 'dayjs';
 
+
+const participantSchema = joi.object({
+    name: joi.string()
+})
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.any().allow('message', 'private_message')
+})
+
+
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/sign-up', (req, res) => {
-    if (!(req.body.username))
-        return res.status(400).send('Todos os campos são obrigatórios');
-    if (!verifyURL(req.body.avatar))
-        return res.status(400).send('URL inválida');
-    users.push(req.body);
-    return res.sendStatus(201);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db('batepapo-uol');
 });
 
-app.post('/tweets', (req, res) => {
+
+app.post('/participants', async (req, res) => {
+    if (!(req.body.name))
+        return res.sendStatus(422);
+    const participant = req.body;
+    const validation = participantSchema.validate(
+        (participant)
+    )
+    if (validation.error) {
+        const errors = validation.error.details
+            .map((detail) => detail.message);
+        console.log(errors);
+        return res.sendStatus(422);
+    }
+    try {
+        const participantFound = await db
+            .collection('participants')
+            .findOne({ name: participant.name });
+        if (participantFound)
+            return res.sendStatus(409);
+        
+        await db.collection('participants')
+            .insertOne({
+                name: participant.name,
+                lastStatus: dayjs(Date.now()).format('HH:mm:ss')
+            })
+        await db.collection('messages')
+            .insertOne({
+                from: participant,
+                to: 'Todos',
+                text: 'entra na sala...',
+                type: 'status',
+                time: dayjs(Date.now()).format('HH:mm:ss')
+            })
+        res.sendStatus(201);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(422);
+    }
+});
+
+app.get('/participants', async (req, res) => {
+    try {
+        const participants = await db
+            .collection('participants')
+            .find()
+            .toArray();
+        res.send(participants);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+})
+app.post('/messages', (req, res) => {
     if (!req.headers.user) return res.status(400).send('Usuário inválido');
     if (!req.body.tweet) return res.status(400).send('Tweet inválido');
     const tweet = {
@@ -27,7 +95,7 @@ app.post('/tweets', (req, res) => {
     return res.status(201).send('Tweet criado');
 });
 
-app.get('/tweets', (req, res) => {
+app.get('/participants', (req, res) => {
     if (req.query.page < 1)
         return res.status(400).send('Informe uma página válida!');
 
@@ -45,12 +113,16 @@ app.get('/tweets', (req, res) => {
 
 });
 
-app.get('/tweets/:USERNAME', (req, res) => {
+app.get('/messages', (req, res) => {
     const tweetsFrom = tweets.filter((tweet) => tweet.username === req.params.USERNAME);
     if (tweetsFrom.length === 0)
         return res.status(404).send('Usuário sem Tweets');
 
     return res.status(200).send(tweetsFrom);
+})
+
+app.post('/status', (req, res) => {
+
 })
 
 app.listen(5000, console.log('Running on port 5000'));
