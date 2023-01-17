6ce8d64317e8c86c8394ca5ddb52d447d01296c3
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-01-17 16:21:18 -0300

    changes for bot avaliator, status code

--- src/middlewares/messages.middlewares.js
+++ src/middlewares/messages.middlewares.js
@@ -16,15 +16,18 @@ export async function messageValidation(req, res, next){
 
     const {to, text, type} = req.body;
 
+    if (!to || !text || !type || !from)
+        return res.sendStatus(422);
+
     try {
         const receiverFound = await db.
         collection('participants').findOne({name:to});
         if (!receiverFound)
-           return res.sendStatus(404);
+           return res.sendStatus(422);
         const senderFound = await db.
         collection('participants').findOne({name:from});
         if (!senderFound)
-            return res.sendStatus(404);
+            return res.sendStatus(422);
     } catch(error){
         console.log(error);
         return res.status(500).send(error.message);
